1aae22465b4fe5e7d354827aae651184b5fe2c6b
Paul Mayer <paul@paul-mayer.eu>
2020-01-07 20:13:05 +0100

    init external config yml

---
+++ internal/config/init.go
@@ -0,0 +1,14 @@
+package config
+
+import (
+	"fmt"
+	"github.com/spf13/viper"
+)
+
+func readConfigFile() {
+	viper.SetConfigName("pohls_config") // name of config file (without extension)
+	viper.AddConfigPath("config")            // optionally look for config in the working directory
+	err := viper.ReadInConfig()         // Find and read the config file
+	if err != nil { // Handle errors reading the config file
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
