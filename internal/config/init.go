189b633237bf5a23bd1ec2f39dd03b9ec57c9ee8
Paul Mayer <paul@paul-mayer.eu>
2020-01-09 17:15:50 +0100

    fix syntax bug

--- internal/config/init.go
+++ internal/config/init.go
@@ -7,8 +7,9 @@ import (
 
 func readConfigFile() {
 	viper.SetConfigName("pohls_config") // name of config file (without extension)
-	viper.AddConfigPath("config")            // optionally look for config in the working directory
+	viper.AddConfigPath("config")       // optionally look for config in the working directory
 	err := viper.ReadInConfig()         // Find and read the config file
 	if err != nil { // Handle errors reading the config file
 		panic(fmt.Errorf("Fatal error config file: %s \n", err))
 	}
+}
